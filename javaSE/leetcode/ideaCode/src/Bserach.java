14f74514411b7a5153203188e7c682f076521f91
hao <hsqzs1996@gmail.com>
2020-01-17 17:28:25 +0800

    添加二分查找算法

--- javaSE/leetcode/ideaCode/src/Bserach.java
+++ javaSE/leetcode/ideaCode/src/Bserach.java
@@ -1,6 +1,44 @@
 /**
+ * 二分查找
  * @author hsqzs
  * date 2020/1/17 16:43
  */
 public class Bserach {
+    public static void main(String[] args) {
+        int [] nums = {3,4,5,9,10,13,15,21,54,68};
+        int len = nums.length;
+        // value 位于第五位
+        int value = 10;
+        System.out.println(bSerach(nums, len, value));
+    }
+    private static int bSerach(int[] list, int n, int value) {
+        int start = 0;
+        int end = n - 1;
+        //while (start <= end) {
+        //    int mid = start + ((end - start) >> 1);
+        //    if (list[mid] == value) {
+        //        return mid + 1;
+        //    }else if (list[mid] > value) {
+        //        end = mid - 1;
+        //    }else {
+        //        start = mid + 1;
+        //    }
+        //}
+        //return -1;
+        return bSerachInternally(list, start, end, value);
+    }
+    //递归
+    private static int bSerachInternally(int[] a, int low, int high, int value) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + ((high - low) >> 1);
+        if (value > a[mid]) {
+            return  bSerachInternally(a, mid + 1, high, value);
+        } else if (value < a[mid]) {
+            return bSerachInternally(a, low, mid - 1, value);
+        }else {
+            return mid + 1;
+        }
+    }
 }
--- javase/leetcode/ideaCode/src/QuickSort.java
+++ javase/leetcode/ideaCode/src/QuickSort.java
@@ -1,19 +1,14 @@
+import java.util.Random;
+
 /**
  * @author hsqzs
  * date 2020/1/10 16:04
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int [] list = {8,2,7,3,11};
+        int [] list = {8,2,7,3,11,3,4,34,2,3,2,78,6,5,4,43,3,3,232,3,23,4,5454,5,6};
         quickSort(list);
-        for (int i : list) {
-            if (i != list[list.length - 1]) {
-                System.out.print(i + ",");
-            }
-            else {
-                System.out.println(i);
-            }
-        }
+        printList(list);
     }
 
     /**
@@ -34,6 +29,11 @@ public class QuickSort {
     }
 
     private static int partition(int [] list, int start, int end) {
+        //使用随机基数
+        int randomNum = (int) ( start + Math.random() * (end - start + 1));
+        int t = list[end];
+        list[end] = list[randomNum];
+        list[randomNum] = t;
         int pivot = list[end];
         int i = start;
         int j = start;
@@ -49,5 +49,17 @@ public class QuickSort {
         list[i] = list[j];
         list[j] = temp;
         return i;
+    }
+    private static void printList(int[] list) {
+        int count = 0;
+        for (int i : list) {
+            ++count;
+            if (count != list.length) {
+                System.out.print(i + ",");
+            }
+            else {
+                System.out.print(i);
+            }
+        }
     }
 }
